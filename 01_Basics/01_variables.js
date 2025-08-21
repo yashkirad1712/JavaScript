af3e2f29f917e78bc5133ca6ce286856ab398b3e
Yanky <115690218+yashkirad1712@users.noreply.github.com>
2025-08-21 10:28:39 +0000

    Basics of JS

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,15 @@
+//Learning the variables
+
+const accountID = 123456
+let accountEmail = "Yash@gmail.com"
+var accountPassword = "1712"
+accountCity = "Pune"
+
+//account ID = 2 //not allowed as accountID is const
+
+accountEmail = "fake@gamil.com"
+accountPassword = "1515"
+accountCity = "Banglore"
+
+console.log(accountID)
+console.table([accountID,accountEmail,accountPassword,accountCity])
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,4 @@
+"use strict"; // Treats all the code in newer version of JS
+
+//alert("Hello") // this is not used in nodejs , only in browser
+
